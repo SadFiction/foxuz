ff97caac3c944028467fc08b1ade1878a078f192
sadfiction <troolface129@gmail.com>
2022-05-30 23:07:40 +0100

    added things

---
+++ foxuz-core/target/debug/deps/foxuz_core-91ddb9420c21da9f.d
@@ -0,0 +1,5 @@
+/home/carlo/Projects/foxuz/foxuz-core/target/debug/deps/foxuz_core-91ddb9420c21da9f: src/lib.rs
+
+/home/carlo/Projects/foxuz/foxuz-core/target/debug/deps/foxuz_core-91ddb9420c21da9f.d: src/lib.rs
+
+src/lib.rs:
---
+++ foxuz-core/target/debug/deps/foxuz_core-e426eafbbb45963b.d
@@ -0,0 +1,7 @@
+/home/carlo/Projects/foxuz/foxuz-core/target/debug/deps/foxuz_core-e426eafbbb45963b.rmeta: src/lib.rs
+
+/home/carlo/Projects/foxuz/foxuz-core/target/debug/deps/libfoxuz_core-e426eafbbb45963b.rlib: src/lib.rs
+
+/home/carlo/Projects/foxuz/foxuz-core/target/debug/deps/foxuz_core-e426eafbbb45963b.d: src/lib.rs
+
+src/lib.rs:
